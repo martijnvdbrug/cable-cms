ccce61d7f02c0401f2f58b9fa072f613f77babd3
martijn <martijn.vandebrug@incentro.com>
2019-05-09 19:08:25 +0200

    try catch for invalid password

--- cable-auth/src/handlers/index.ts
+++ cable-auth/src/handlers/index.ts
@@ -1,19 +1,28 @@
 import express = require('express');
-import {Env} from '../env';
+import {UserService} from '../user/user.service';
+
 require('dotenv').config();
 
 export const authHandler = express();
 
-authHandler.post('/auth', (req, res) => {
+authHandler.post('/auth', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   if (!password || !email) {
-    res.send(500, `email and password are needed to authenticate!`)
+    res.send(500, `email and password are needed to authenticate!`);
   }
-  return res.send({
-    token: `${Env.token}`,
-    provider: 'github'
-  });
+  try {
+    const token = await UserService.getToken(email, password);
+    return res.send({
+      token: token,
+      provider: 'github'
+    });
+  } catch (err) {
+    console.error(`Error authentication email ${email} with password ${password}`);
+    res.send(500, {
+      error: err
+    });
+  }
 });
 
 authHandler.get('/', (req, res) => {
